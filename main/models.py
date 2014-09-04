9e4b587232a0f033949238d30667f0f1f9070c2c
J. Kevin Bay <seregon@gmail.com>
2014-09-03 21:56:13 -0500

    Starter model definitions

--- main/models.py
+++ main/models.py
@@ -1,3 +1,25 @@
+"""
+High-level containers and document types
+"""
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
 
-# Create your models here.
+
+class BaseElement(MPTTModel):
+    """
+    The base class for nodes in various tree structures.
+
+    Provides MPTT-inherited features and basic funtionality
+    """
+    class MPTTMeta:
+        order_insertion_by = ('index',)
+
+    parent = TreeForeignKey('self', null=True, blank=True,
+                            related_name='children')
+    index = models.PositiveSmallIntegerField(unique=True)
+
+
+class Document(models.Model):
+    """Generic container for a blank template"""
+    name = models.CharField(max_length=255)
+    elements = models.ManyToManyField(BaseElement, related_name='documents')
