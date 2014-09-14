b92506dcc3b76747ae4b969a8e561458dc0197ae
J. Kevin Bay <seregon@gmail.com>
2014-09-13 20:24:06 -0500

    Refactored various filters into one file (wysiweb_filters)

--- main/templatetags/wysiweb_filters.py
+++ main/templatetags/wysiweb_filters.py
@@ -5,6 +5,29 @@ from markdown import markdown as convert_markdown
 
 register = template.Library()
 
+
+@register.filter
+def elemid(obj):
+    """
+    Return a unique identifier for this element.
+    """
+    return '%s-%s' % (obj.__class__.__name__, obj.pk)
+
+
+@register.filter
+def dictaccess(d, key):
+    """
+    Access dict value by key.
+
+    This allows for dict lookups using template variables, which Django's
+    templates do not support natively.
+    """
+    try:
+        return d[key]
+    except KeyError:
+        return None
+
+
 @register.filter
 @stringfilter
 def markdown(text, safe_mode='escape'):
@@ -29,3 +52,14 @@ def markdown(text, safe_mode='escape'):
         'html_replacement_text': '',
     }
     return mark_safe(convert_markdown(text, **options))
+
+
+@register.filter
+def render(node):
+    """
+    Call a SelfRendering node's render method with the specified indent level.
+    """
+    try:
+        return mark_safe(node.render(indent=node.level+1))
+    except AttributeError:
+        return ''
